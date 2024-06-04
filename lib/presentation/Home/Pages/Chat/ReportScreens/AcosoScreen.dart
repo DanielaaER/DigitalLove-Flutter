7e20267116e87c75ba22fdc46cbf84ff86cdfb24
KevinPozos <kevin_pozos@outlook.com>
2024-06-04 11:27:28 -0600

    fix

---
+++ lib/presentation/Home/Pages/Chat/ReportScreens/AcosoScreen.dart
@@ -0,0 +1,103 @@
+import 'package:digital_love/shared/widgets/Button.dart';
+import 'package:digital_love/shared/widgets/TextBold.dart';
+import 'package:digital_love/shared/widgets/TextField.dart';
+import 'package:digital_love/shared/widgets/TextFieldEmail.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+import '../../../../../config/theme/app_colors.dart';
+
+class AcosoScreen extends StatefulWidget {
+  @override
+  _AcosoScreenState createState() => _AcosoScreenState();
+}
+
+class _AcosoScreenState extends State<AcosoScreen> {
+  TextEditingController _textController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void _update(String update) {}
+
+  @override
+  Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+
+    var title = width * 0.07;
+    var text = width * 0.05;
+
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(
+          color: AppColors.whiteColor,
+          height: MediaQuery.of(context).size.height * .8,
+          width: width,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                  padding: EdgeInsets.only(top: height * .1, left: width * .1),
+                  child: GestureDetector(
+                      onTap: () {},
+                      child: Icon(
+                        Icons.arrow_back_ios,
+                        size: title,
+                      ))),
+              Padding(
+                padding: EdgeInsets.only(top: height * .05, left: width * .1),
+                child: CustomTextBold(
+                    textValue: "Reportar",
+                    size: title,
+                    color: AppColors.backColor),
+              ),
+              SizedBox(height: height * .05),
+              Container(
+                width: width,
+                decoration: const BoxDecoration(
+                  border: Border(
+                    top: BorderSide(
+                      color: AppColors.backColor,
+                      width: 1,
+                      style: BorderStyle.solid,
+                    ),
+                    bottom: BorderSide(
+                      color: AppColors.backColor,
+                      width: 1,
+                      style: BorderStyle.solid,
+                    ),
+                  ),
+                ),
+                child: Column(
+                  // crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: width * .8,
+                      transformAlignment: Alignment.topCenter,
+                      padding: EdgeInsets.only(
+                          top: height * .05, bottom: height * .05),
+                      child: CustomTextField(
+                          textValue: "Reportar", controller: _textController),
+                    ),
+                    Container(
+                      width: width * .5,
+                      transformAlignment: Alignment.topCenter,
+                      padding: EdgeInsets.only(bottom: height * .05),
+                      child: CustomButton(
+                        textValue: "Reportar",
+                        onPressed: () {
+                          _update(_textController.text);
+                        },
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+}
---
+++ lib/presentation/Home/Pages/Chat/widgets/Report.dart
@@ -0,0 +1,181 @@
+import 'package:digital_love/config/theme/app_colors.dart';
+import 'package:digital_love/presentation/Home/Pages/Account/Pages/age.dart';
+import 'package:digital_love/presentation/Home/Pages/Account/Pages/city.dart';
+import 'package:digital_love/presentation/Home/Pages/Account/Pages/gender.dart';
+import 'package:digital_love/presentation/Home/Pages/Account/Pages/lastname.dart';
+import 'package:digital_love/presentation/Home/Pages/Account/Pages/name.dart';
+import 'package:digital_love/presentation/Home/Pages/Account/Pages/sex.dart';
+import 'package:digital_love/presentation/Home/Pages/Account/widgets/setting.dart';
+import 'package:digital_love/presentation/Home/Pages/Chat/ReportScreens/AcosoScreen.dart';
+import 'package:digital_love/presentation/Home/Pages/Chat/ReportScreens/BullyingScreen.dart';
+import 'package:digital_love/presentation/Home/Pages/Chat/ReportScreens/DiscriminacionScreen.dart';
+import 'package:digital_love/presentation/Home/Pages/Chat/ReportScreens/EstafaScreen.dart';
+import 'package:digital_love/presentation/Home/Pages/Chat/ReportScreens/IdentidadScreen.dart';
+import 'package:digital_love/presentation/Home/Pages/Chat/ReportScreens/OtrosScreen.dart';
+import 'package:digital_love/presentation/Home/Pages/Settings/Pages/cookies.dart';
+import 'package:digital_love/presentation/Home/Pages/Settings/Pages/email.dart';
+import 'package:digital_love/presentation/Home/Pages/Settings/Pages/password.dart';
+import 'package:digital_love/presentation/Home/Pages/Settings/Pages/preferences.dart';
+import 'package:digital_love/presentation/Home/Pages/Settings/Pages/privacity.dart';
+import 'package:digital_love/presentation/Login/Login.dart';
+import 'package:digital_love/shared/services/AuthServices.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+import '../ReportScreens/LenguajeScreen.dart';
+
+class ReportScreen extends StatefulWidget {
+  @override
+  _ReportScreenState createState() => _ReportScreenState();
+}
+
+class _ReportScreenState extends State<ReportScreen> {
+  ScrollController _scrollController = ScrollController();
+
+  void _loadData() {
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    _loadData();
+    _scrollController.addListener(_scrollListener);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_scrollListener);
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _scrollListener() {
+    if (_scrollController.position.userScrollDirection ==
+        ScrollDirection.reverse) {
+      setState(() {});
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+
+    var title = width * 0.07;
+    var text = width * 0.05;
+
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(
+          color: AppColors.whiteColor,
+          height: MediaQuery.of(context).size.height * .8,
+          width: width,
+          child: SingleChildScrollView(
+            controller: _scrollController,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(width * .1),
+                  child: Text(
+                    "Reportar",
+                    style:
+                        TextStyle(fontSize: title, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                // SizedBox(height: height * .02),
+                Container(
+                  decoration: const BoxDecoration(
+                    border: Border(
+                      top: BorderSide(
+                        color: AppColors.backColor,
+                        width: 1,
+                        style: BorderStyle.solid,
+                      ),
+                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SettingWidget(
+                        setting: "Acoso",
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => AcosoScreen(),
+                              ));
+                        },
+                      ),
+                      SettingWidget(
+                        setting: "Lenguaje inapropiado",
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => LenguajeScreen(),
+                              ));
+                        },
+                      ),
+                      SettingWidget(
+                        setting: "Estafa o fraude",
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => EstafaScreen(),
+                              ));
+                        },
+                      ),
+                      SettingWidget(
+                        setting: "Bullying",
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => BullyingScreen(),
+                              ));
+                        },
+                      ),
+                      SettingWidget(
+                        setting: "Suplantación de identidad",
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => IdentidadScreen(),
+                              ));
+                        },
+                      ),
+                      SettingWidget(
+                        setting: "Racismo o discriminación",
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => DiscriminacionScreen(),
+                              ));
+                        },
+                      ),
+                      SettingWidget(
+                        setting: "Otros",
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => OtroScreen(),
+                              ));
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // ),
+        ));
+  }
+}
